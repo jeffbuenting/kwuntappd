f3330fe6069cefd04af8ad59fa1464431b2717a7
jeff buenting <jeffbuenting@gmail.com>
2026-01-08 17:02:01 -0600

    commercial agreement required

--- getbeers.py
+++ getbeers.py
@@ -1,10 +1,17 @@
-# main.py
+import json
+import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv() # take environment variables from .env.
+
+# main.py
+
+# take environment variables from .env.
+load_dotenv() 
 
 api_key = os.getenv("API_KEY")
 db_password = os.getenv("DB_PASSWORD")
 
 print(f"API Key: {api_key}")
+
+
